b2d14c9f6ec880b557b63ace7e3d4f97c13bb12a
Alex Mourão <amouraorr@yahoo.com.br>
2025-10-29 16:09:49 -0300

    implementações e ajustes

--- src/main/java/com/fiap/apigateway/config/swagger/OpenApiSwaggerConfig.java
+++ src/main/java/com/fiap/apigateway/config/swagger/OpenApiSwaggerConfig.java
@@ -2,18 +2,20 @@ package com.fiap.apigateway.config.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiSwaggerConfig {
 
     @Bean
+    @ConditionalOnMissingBean(OpenAPI.class)
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("PÓS GRADUAÇÃO - FIAP 2025 - SERVIÇO API Gateway")
-                        .version("1.0.0")
+                        .version("v1")
                         .description("API Gateway responsável pelo roteamento, autenticação (JWT/OAuth2), políticas de segurança e documentação (OpenAPI) do sistema de gerenciamento de encomendas."));
     }
 }
---
+++ src/main/java/com/fiap/apigateway/infrastructure/config/security/SecurityConfigProd.java
@@ -0,0 +1,32 @@
+package com.fiap.apigateway.infrastructure.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@Profile("prod")
+@EnableWebFluxSecurity
+public class SecurityConfigProd {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable()) 
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(
+                                "/actuator/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/health/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+        return http.build();
+    }
+}
